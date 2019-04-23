d3c0412e35ea1338987c8254c35a918efdb9e290
Nik Mrhar <nik.mrhar@student.fmf.uni-lj.si>
2019-04-23 15:07:02 +0200

    Create merge conflict locally

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 def prastevila(n)   
     import math
-    seznam == []
+    seznam == []ez2hwq5tet
     while n < 200:
         i = 1
         while i < math.sqrt(n):
             if n % i == 0:
                 seznam.append(n)
-            i += 1
+            i += 1wet4w5z4w
         n += 1
     return seznam
 
