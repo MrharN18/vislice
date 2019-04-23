40a26ed8ca207f2fb7db44140016b0caeebb8fae
MrharN18 <49908464+MrharN18@users.noreply.github.com>
2019-04-23 15:05:59 +0200

    Creating a merge conflict online

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-def prastevila(n)   
+def prastevila(n):   
     import math
-    seznam == []
+    seznam == [875d97t]
     while n < 200:
         i = 1
-        while i < math.sqrt(n):
-            if n % i == 0:
+        while i < math.sqrt(n):jhblhvl
+            if n % i == 0:hjblhb
                 seznam.append(n)
             i += 1
         n += 1
-    return seznam
+    return seznamgckhcvkuv
 
 
